a607f0613b6efa16b1b0c4d1970e1b7fd25280a6
unknown <sntakirutimana72@gmail.com>
2022-08-11 14:41:53 +0200

    Add sql command for initiating schema and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic;
+
+\c vet_clinic;
+
+CREATE TABLE IF NOT EXISTS animals(
+  id              int GENERATED ALWAYS AS IDENTITY, 
+  name            varchar(100) not null,
+  date_of_birth   date not null,
+  escape_attempts int not null,
+  neutered        boolean not null,
+  weight_kg       float not null,
+  PRIMARY KEY(id)
 );
