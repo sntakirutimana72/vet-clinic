a615dfc8fef3fd51e9b5af2622f10153007ead53
unknown <sntakirutimana72@gmail.com>
2022-08-12 16:49:22 +0200

    Update schema.sql file with queries to add more tables

--- schema.sql
+++ schema.sql
@@ -4,14 +4,35 @@ CREATE DATABASE vet_clinic;
 
 \c vet_clinic;
 
-CREATE TABLE IF NOT EXISTS animals(
-  id              int GENERATED ALWAYS AS IDENTITY, 
-  name            varchar(100) not null,
-  date_of_birth   date not null,
-  escape_attempts int not null,
-  neutered        boolean not null,
-  weight_kg       float not null,
+CREATE TABLE IF NOT EXISTS owners (
+  id        INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(255) NOT NULL,
+  age       INT NOT NULL,
   PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD species varchar(40); 
+CREATE TABLE IF NOT EXISTS species (
+  id   INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(40) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS animals (
+  id              INT GENERATED ALWAYS AS IDENTITY, 
+  name            VARCHAR(100) NOT NULL,
+  species_id      INT,
+  owner_id        INT,
+  date_of_birth   DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered        BOOLEAN NOT NULL,
+  weight_kg       FLOAT NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_species
+    FOREIGN KEY(species_id) 
+      REFERENCES species(id) 
+      ON DELETE SET NULL,
+  CONSTRAINT fk_owners
+    FOREIGN KEY(owner_id) 
+      REFERENCES owners(id) 
+      ON DELETE SET NULL
+);
