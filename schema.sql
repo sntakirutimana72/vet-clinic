671866e70d5dbeed2309ca768d38f21fa757c832
unknown <sntakirutimana72@gmail.com>
2022-08-13 12:38:18 +0200

    Apply required changes from review

--- schema.sql
+++ schema.sql
@@ -20,19 +20,17 @@ CREATE TABLE IF NOT EXISTS species (
 CREATE TABLE IF NOT EXISTS animals (
   id              INT GENERATED ALWAYS AS IDENTITY, 
   name            VARCHAR(100) NOT NULL,
-  species_id      INT,
-  owner_id        INT,
   date_of_birth   DATE NOT NULL,
   escape_attempts INT NOT NULL,
   neutered        BOOLEAN NOT NULL,
   weight_kg       FLOAT NOT NULL,
-  PRIMARY KEY(id),
-  CONSTRAINT fk_species
-    FOREIGN KEY(species_id) 
-      REFERENCES species(id) 
-      ON DELETE SET NULL,
-  CONSTRAINT fk_owners
-    FOREIGN KEY(owner_id) 
-      REFERENCES owners(id) 
-      ON DELETE SET NULL
+  PRIMARY KEY(id)
 );
+
+ALTER TABLE animals ADD species VARCHAR(40);
+
+BEGIN;
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD species_id INT REFERENCES species (id);
+ALTER TABLE animals ADD owner_id INT REFERENCES owners (id);
+COMMIT;
